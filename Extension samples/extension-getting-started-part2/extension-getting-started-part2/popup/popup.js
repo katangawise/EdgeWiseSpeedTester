51c2e34b798f4a3264240c1df8482ff498702574
katangawise <katangawise@users.noreply.github.com>
2023-04-16 17:14:18 +0300

    test

--- Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/popup.js
+++ Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/popup.js
@@ -1,24 +1,33 @@
-const sendMessageId = document.getElementById("sendmessageid");
-if (sendMessageId) {
-    sendMessageId.onclick = function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                    url: chrome.runtime.getURL("images/stars.jpeg"),
-                    imageDivId: `${guidGenerator()}`,
-                    tabId: tabs[0].id
-                },
-                function(response) {
-                    window.close();
-                }
-            );
-            function guidGenerator() {
-                const S4 = function () {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-            }
-        });
-    };
-}
+// const sendMessageId = document.getElementById("sendmessageid");
+// if (sendMessageId) {
+//     sendMessageId.onclick = function() {
+//         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//             chrome.tabs.sendMessage(
+//                 tabs[0].id,
+//                 {   
+//                     url: chrome.runtime.getURL("https://www.advancedsciencenews.com/wp-content/uploads/2022/08/Carina_Webb.jpg"),
+//                     imageDivId: `${guidGenerator()}`,
+//                     tabId: tabs[0].id
+//                 },
+//                 function(response) {
+//                     window.close();
+//                 }
+//             );
+//             function guidGenerator() {
+//                 const S4 = function () {
+//                     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+//                 };
+//                 return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+//             }
+//         });
+//     };
+// }
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    console.log("-------");
+    var activeTab = tabs[0];
+    var activeTabId = activeTab.id; // or do whatever you need
+    alert("The url is: " + activeTab.url);
+  });
